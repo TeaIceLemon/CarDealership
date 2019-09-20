63575bdaafc9b29fe04c0d0ff1c2888d9c89a89d
TeaIceLemon <piotr.wieczorek1993@gmail.com>
2019-09-20 22:13:58 +0200

    Finished POJO classes; Finished pojo class mapping

--- src/main/java/pojo/Vehicle.java
+++ src/main/java/pojo/Vehicle.java
@@ -1,13 +1,28 @@
 package pojo;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "vehicles")
 public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
+    @Column(name = "VehicleID")
+    private int id;
+    @Column(name = "VehicleMake")
+    private String vMake;
+    @Column(name = "VehicleModel")
+    private String vModel;
+    @Column(name = "VehicleYear")
+    private int vYear;
+    @Column(name = "NewOrUsed")
+    private boolean newOrUsed;
+    @Column(name = "VehiclePrice")
+    private BigDecimal vPrice;
+    @Column(name = "VehicleNotes")
+    private String vNotes;
+    @Column(name = "LocationID")
+    private int lId;
 
 }
